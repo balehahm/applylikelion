3f3694d5c6ec442870b3e0bd424175b55a531155
starius <okm50088@naver.com>
2015-11-11 02:51:04 +0000

    basic CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,45 @@
 class PostsController < ApplicationController
   def index
+    @posts =Post.all
   end
 
   def new
+    
   end
 
   def create
+    post = Post.new
+    post.name = params[:post][:name]
+    post.email = params[:post][:email]
+    post.password = params[:post][:password]
+    post.content = params[:post][:content]
+    post.save
+    redirect_to posts_path
   end
 
   def show
+      redirect_to posts_path
   end
 
   def edit
+      @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.name = params[:post][:name]
+    post.email = params[:post][:email]
+    post.password = params[:post][:password]
+    post.content = params[:post][:content]
+    post.save
+    redirect_to posts_path
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
+  
+
 end
